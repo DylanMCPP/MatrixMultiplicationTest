41c8ca67136bb9266ee9fdf956369b4ded76af9e
Dylan Michalak <dwmichalak@cpp.edu>
2023-04-03 22:44:47 -0700

    style: format algorithms class with placeholder methods and javadoc comments

---
+++ MatrixAlgorithms.java
@@ -0,0 +1,40 @@
+// Dylan Michalak
+// CS 3310-03
+// Project 1: Matrix Multiplication
+// Due: 4/6/2023
+
+public class MatrixAlgorithms {
+
+    /**
+     * This method takes two 2d int arrays of some size 2^k, where k is a positive integer, and returns
+     * the product of their multiplication, using a classical matrix multiplication algorithm
+     * @param arrayA the first multiplicand
+     * @param arrayB the second multiplicand
+     * @return
+     */
+    public static int[][] classicalMult(int[][] arrayA, int[][] arrayB) {
+        return null;
+    }
+    
+    /**
+     * This method takes two 2d int arrays of some size 2^k, where k is a positive integer, and returns
+     * the product of their multiplication, using a divide-and-conquer approach matrix multiplication algorithm
+     * @param arrayA the first multiplicand
+     * @param arrayB the second multiplicand
+     * @return
+     */
+    public static int[][] divAndConqMult(int[][] arrayA, int[][] arrayB) {
+        return null;
+    }
+
+    /**
+     * This method takes two 2d int arrays of some size 2^k, where k is a positive integer, and returns
+     * the product of their multiplication, using Strassen's matrix multiplication algorithm
+     * @param arrayA the first multiplicand
+     * @param arrayB the second multiplicand
+     * @return
+     */
+    public static int[][] strassensMult(int[][] arrayA, int[][] arrayB) {
+        return null;
+    }
+}
