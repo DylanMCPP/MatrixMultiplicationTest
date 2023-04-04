0042f4500e9c6a29854d42c3bd6c9505b0fe5b38
Dylan Michalak <dwmichalak@cpp.edu>
2023-04-03 23:38:13 -0700

    feat: add helper method for divide and conquer approach
    This change adds a helper method which divides a given 2^k size square
    2d array into 4 quartiles, and returns a 4-layer 3d array, with a
    different quartile of the original stored in each layer

--- MatrixAlgorithms.java
+++ MatrixAlgorithms.java
@@ -47,8 +47,59 @@ public class MatrixAlgorithms {
      * @param arrayB the second multiplicand
      * @return
      */
-    public static int[][] divAndConqMult(int[][] arrayA, int[][] arrayB) {
+    public static int[][] divAndConqMult(int[][] matrixA, int[][] matrixB) {
         return null;
+    }
+
+    /*
+     * internal helper method that divides a 2d, square, 2^k size array into 4 quartiles,
+     * and returns a 3d array containing a different quartile in each of its 4 layers
+     */
+    private int[][][] matrixSplit(int[][] startMatrix) {
+
+        // define newSize variable equal to half the width/height of the starting square matrix
+        int newSize = startMatrix.length / 2;
+
+        // define a 3d array, that can store 4 newSize x newSize matrices
+        int[][][] newMatrix = new int[newSize][newSize][4];
+
+        // Quartile 1 | Quartile 2
+        // -----------|------------
+        // Quartile 3 | Quartile 4
+        for (int quartile = 1; quartile <=4; quartile++) {
+            switch(quartile) {
+
+                // stores the first matrix quartile as a 2d array in the first layer of the 3d matrix
+                case 1:
+                    for (int i = 0; i < newSize; i++) {
+                        for (int j = 0; j < newSize; j++) {
+                            newMatrix[i][j][quartile] = startMatrix[i][j];
+                        }
+                    } break;
+                // stores the second matrix quartile as a 2d array in the second layer of the 3d matrix
+                case 2:
+                    for (int i = 0; i < newSize; i++) {
+                        for (int j = 0; j < newSize; j++) {
+                            newMatrix[i][j][quartile] = startMatrix[i][j + newSize];
+                        }
+                    } break;
+                // stores the third matrix quartile as a 2d array in the third layer of the 3d matrix
+                case 3:
+                    for (int i = 0; i < newSize; i++) {
+                        for (int j = 0; j < newSize; j++) {
+                            newMatrix[i][j][quartile] = startMatrix[i + newSize][j];
+                        }
+                    } break;
+                // stores the fourth matrix quartile as a 2d array in the first layer of the 3d matrix
+                case 4:
+                    for (int i = 0; i < newSize; i++) {
+                        for (int j = 0; j < newSize; j++) {
+                            newMatrix[i][j][quartile] = startMatrix[i + newSize][j + newSize];
+                        }
+                    } break;
+            }
+        }
+        return newMatrix;
     }
 
     /**
@@ -58,7 +109,7 @@ public class MatrixAlgorithms {
      * @param arrayB the second multiplicand
      * @return
      */
-    public static int[][] strassensMult(int[][] arrayA, int[][] arrayB) {
+    public static int[][] strassensMult(int[][] matrixA, int[][] matrixB) {
         return null;
     }
 }
