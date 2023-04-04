34716b3881e18c014d6bd85e56e8e14e28957919
Dylan Michalak <dwmichalak@cpp.edu>
2023-04-03 23:01:19 -0700

    feat: implement classical matrix multiplication algorithm with comments

--- MatrixAlgorithms.java
+++ MatrixAlgorithms.java
@@ -12,8 +12,32 @@ public class MatrixAlgorithms {
      * @param arrayB the second multiplicand
      * @return
      */
-    public static int[][] classicalMult(int[][] arrayA, int[][] arrayB) {
-        return null;
+    public static int[][] classicalMult(int[][] matrixA, int[][] matrixB) {
+
+        int size = matrixA.length; // initializes a variable equal to "n", in the given n x n matrices
+
+        int[][] finalMatrix = new int[size][size];
+
+        // the first two loops go to each spot in the final product matrix
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size; j++) {
+
+                int dotProduct = 0; // initializes a dot product variable at 0
+
+                /* the final loop calculates, for every location in the product matrix, 
+                the dot product of that row in the first multiplicand matrix, and that 
+                column in the second multiplicand matrix*/
+                for (int k = 0; k < size; k++) {
+                    dotProduct += matrixA[i][k] * matrixB[k][j];
+                }
+
+                // store each dot product in its appropriate location
+                finalMatrix[i][j] = dotProduct;
+            }
+        }
+        // return the product matrix
+        return finalMatrix;
     }
     
     /**
